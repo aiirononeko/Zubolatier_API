a4dd2cd40b533c29d8f88241210d5b4ac305bc84
片田凌太 <aiirononeko@gmail.com>
2020-04-20 23:21:02 +0900

    Cloud FireStore初期化処理を実装

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/aiirononeko/Zubolatier_API
 go 1.14
 
 require (
+	cloud.google.com/go/firestore v1.2.0 // indirect
+	firebase.google.com/go v3.12.0+incompatible // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
@@ -12,4 +14,5 @@ require (
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
 	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
 	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/api v0.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 	"os"
 
+	firebase "firebase.google.com/go"
 	"github.com/labstack/echo"
+	"google.golang.org/api/option"
 )
 
 func main() {
+
+	// Cloud FireStoreの初期化
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("path/to/serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
 	// サーバーのインスタンスを作成
 	e := echo.New()
 
@@ -16,6 +35,7 @@ func main() {
 
 	// サーバー起動
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+
 }
 
 func hello(c echo.Context) error {
